6bb881ea9d71558bdef30ad2f634e2f2d4f54674
= <=>
2015-05-11 12:23:49 -0400

    Initial Commit

---
+++ cedargittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cedargittest
+//
+//  Created by pairing on 5/11/15.
+//  Copyright (c) 2015 pairing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cedargittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cedargittest
+//
+//  Created by pairing on 5/11/15.
+//  Copyright (c) 2015 pairing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
